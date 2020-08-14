3cf026cd9482633b5a2dd021e9433a6eae620dbc
Ipung Purwono <ipungofficial@gmail.com>
2020-08-14 16:15:07 +0700

    Initial Commit

---
+++ ChalengeDay02/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChalengeDay02
+//
+//  Created by Ipung Dev Center on 14/08/20.
+//  Copyright © 2020 Banyu Center. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
